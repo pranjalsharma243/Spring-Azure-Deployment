11e17c8c81f426ef021581669aba2352cc43b45c
pranjsharma243 <pranjalsharma243@gmail.com>
2023-08-12 01:01:48 +0200

    Spring deployment

---
+++ src/main/java/com/SpringDeployApp/Myapp/SpringDeployAppApplication.java
@@ -0,0 +1,13 @@
+package com.SpringDeployApp.Myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeployAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeployAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/SpringDeployApp/Myapp/SpringDeployAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringDeployApp.Myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
